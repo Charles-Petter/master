4bc4a0f217781a79749df26bdac249c487971842
cxtop <1748521208@qq.com>
2022-12-02 22:19:31 +0800

    12/2

---
+++ go项目/cx-ser-v1/settings/settings_cx.go
@@ -0,0 +1,26 @@
+package settings
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"os"
+)
+//viper 管理文件
+func InitConfig() (v_cx *viper.Viper){
+	workDir_cx, _ := os.Getwd()
+	v_cx = viper.New()
+	v_cx.SetConfigName("application")
+	v_cx.SetConfigType("yml")
+	v_cx.AddConfigPath(workDir_cx + "/Config")
+	err := v_cx.ReadInConfig()
+	if err != nil {
+		fmt.Printf("读取配置文件出错：%s\n", err)
+	}
+	//监控配置文件变化
+		v_cx.WatchConfig()
+		v_cx.OnConfigChange(func(in fsnotify.Event) {
+			fmt.Println("配置文件修改了...")
+		})
+	return v_cx
+}
